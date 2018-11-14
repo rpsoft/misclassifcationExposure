f9e96b5797aa330fa60001a336cedf2c41d86a5a
Megan McMinn <megan.mcminn@glasgow.ac.uk>
2018-11-14 10:38:03 +0000

    adding context

--- app.R
+++ app.R
@@ -19,7 +19,7 @@ ui <- fluidPage(
    
    # Application title
   h2("Non-differential misclassification of exposure status in a hypothetical case-control study"),
-  p("Rothman, K.J., 2012", em("Eidemiology: an introduction"), ", Oxford University Press. (p. 135)"),
+  p("Rothman, K.J., 2012", em("Epidemiology: an introduction"), ", Oxford University Press. (p. 135)"),
   p("This table reprents hypothetical data for a Case-Control study investigating the effects of a high fat diet on the odds of having a subsequent Myocardial infarction (MI). 
     The participant's diets are recorded, and classified as 'high fat' (exposed) or 'not' (unexposed). As there will be an arbitrary cut-off point at which a diet is regarded as being 'high fat',
     it is plausible that some of the participants will have their exposure status misclassified. "),
@@ -42,38 +42,37 @@ ui <- fluidPage(
       ),
       
       mainPanel(
-          textOutput("explan"),
+        conditionalPanel(
+          condition = "input.exp == 0 && input.unexp  == 0",
+          p("This first table shows the situation where all subjects are correctly classified with respect 
+            to the outcome (i.e. MI), and the exposure (i.e. eating a high fat diet). In reality, this distribtion is almost never truely known.",
+            br(),
+            "Change the sliders on the left to adjust the amount of misclassification there is from high fat diet to non, and vice versa.")
+          ),
+        conditionalPanel(
+          condition = "input$exp != 0 || input$unexp  != 0",
+          p("Note that when you change the misclassification sliders, only the row totals change - the column totals of those classified as an MI Case or Control do not change.")
+        ),
+        
+
           DT::dataTableOutput('table'),
           textOutput("odds"),
-          textOutput("fisher")
+          textOutput("fisher"),
+          p(br(),
+            br(),
+            "See how the following situations changes the odds ratio:",
+            br(),
+            "- 20% of unexposed misclassified as exposed",
+            br(),
+            "- 20% of exposed misclassified as unexposed",
+            br(),
+            "- 20% misclassified in both directions")
       )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   # output$distPlot <- renderPlot({
-   #    # generate bins based on input$bins from ui.R
-   #    x    <- faithful[, 2] 
-   #    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-   #    
-   #    # draw the histogram with the specified number of bins
-   #    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   # })
-  
-
-    output$explan <- renderText({
-      a <- input$exp / 100
-      b <- input$unexp / 100
-      if (a== 0 & b == 0) {
-        paste0("This first table shows the situation where all subjects are correctly classified with respect 
-            to the outcome (i.e. MI), and the exposure (i.e. eating a high fat diet).
-            In reality, this distribtion is almost never truely known.")
-      } else {
-        paste0("")
-      }
-    })
-    
     
     output$table <- DT::renderDataTable({
       a <- input$exp / 100
